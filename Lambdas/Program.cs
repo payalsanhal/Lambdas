babefc39c0180388c9c4e6bb1b28b91317b30870
payalsanhal <payalsanhal@gmail.com>
2020-01-22 10:15:59 -0500

    Comment added

--- Lambdas/Program.cs
+++ Lambdas/Program.cs
@@ -19,26 +19,29 @@ public class Program
 
     public static void Main()
     {
+        AnotherMain();
+
         Func<int, int> g = x => x + 3;
         int p = g(7);
+        Console.WriteLine("sample 1 : " + p);
+
         del myDelegate = x => x * x;
         int j = myDelegate(5);
-        Console.WriteLine(j + " " + p);
+        Console.WriteLine("sample 2 : " + p);
 
         Func<int> f = () => 20; 
-        Console.WriteLine(f());
+        Console.WriteLine("sample 3 : "+f());
 
         Func<int> f1= () => Cal();
-        Console.WriteLine(f1());
+        Console.WriteLine("sample 4 : "+f1());
 
         Action<int> ff = x => { Cal(x); };
         ff(3);
-        //comment
 
         Action<string> myDel = n =>
         {
          string s = n + " " + "World";
-         Console.WriteLine(s);
+         Console.WriteLine("sample 6 : "+s);
         };
             myDel("Hello");
     }
@@ -51,32 +54,32 @@ public class Program
     static int Cal(int x)
     {
         int result = x * 5;
-        Console.WriteLine(result);
-
+        Console.WriteLine("sample 5 : "+result);
+        //Action void return type so need to write in method
         return result;
     }
 
-    //delegate double delegateDouble(double d);
+    delegate double delegateDouble(double d);
 
-    //public static void Main()
-    //{
+    public static void AnotherMain()
+    {
 
-    //	int someVal = 7;
+        int someVal = 7;
 
-    //	delegateDouble myDelegate = x => x + x;
-    //	Console.WriteLine("Lambda myDelegate = {0}", myDelegate(10));
+        delegateDouble myDelegate = x => x + x;
+        Console.WriteLine("Lambda myDelegate = {0}", myDelegate(10));
 
 
-    //	// delegate f points to lambda
-    //	Func<int, int> f = x => x + someVal;
+        // delegate f points to lambda
+        Func<int, int> f = x => x + someVal;
 
-    //	Console.WriteLine("Lambda f = {0}", f(3));
+        Console.WriteLine("Lambda f = {0}", f(3));
 
-    //	Func<int, int, int> g = (x, y) => x + y + someVal;
+    	Func<int, int, int> g = (x, y) => x + y + someVal;
 
-    //	Console.WriteLine("Lambda g = {0}", g(2, 2));
+    Console.WriteLine("Lambda g = {0}", g(2, 2));
 
-    //	Func<int, int, double> multi = (a, b) => a * b ;
-    //	Console.WriteLine($"Result is = { multi(25,2)}");
-    //}
+    	Func<int, int, double> multi = (a, b) => a * b;
+    Console.WriteLine($"Result is = { multi(25,2)}");
+    }
 }
